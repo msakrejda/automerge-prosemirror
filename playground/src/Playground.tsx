05ef431f242f311567c951e127f9d594e76e7456
Maciek Sakrejda <m.sakrejda@gmail.com>
2025-12-19 14:15:05 -0800

    Add debug view to Playground

--- playground/src/Playground.tsx
+++ playground/src/Playground.tsx
@@ -13,6 +13,7 @@ import {
 import { Mark, Node } from "prosemirror-model"
 import { BlockMarker } from "../../src/types"
 import * as Automerge from "@automerge/automerge"
+import { EditorState } from "prosemirror-state"
 
 // @ts-expect-error this is for debug
 window.A = Automerge
@@ -66,6 +67,7 @@ function Playground({ demoMode }: Props) {
       leftHandle={leftHandle}
       rightHandle={rightHandle}
       showTitle={!demoMode}
+      showDebug={!demoMode}
     />
   )
 
@@ -85,6 +87,7 @@ function Playground({ demoMode }: Props) {
             leftHandle={leftHandle}
             rightHandle={rightHandle}
             showTitle={true}
+            showDebug={true}
           />
         ),
       },
@@ -112,27 +115,30 @@ type TabProps = {
   leftHandle: DocHandle<{ text: string }>
   rightHandle: DocHandle<{ text: string }>
   showTitle: boolean
+  showDebug: boolean
 }
 
-function SameSchema({ leftHandle, rightHandle, showTitle }: TabProps) {
+function SameSchema({ leftHandle, rightHandle, showTitle, showDebug }: TabProps) {
   return (
     <div>
       {showTitle && <h2>Same Schema</h2>}
       <div id="editors">
         <div className="editor">
-          <Editor
+          <DebugEditor
             name="left"
             handle={leftHandle}
             path={["text"]}
             schemaAdapter={basicSchemaAdapter}
+            debug={showDebug}
           />
         </div>
         <div className="editor">
-          <Editor
+          <DebugEditor
             name="right"
             handle={rightHandle}
             path={["text"]}
             schemaAdapter={basicSchemaAdapter}
+            debug={showDebug}
           />
         </div>
       </div>
@@ -140,30 +146,84 @@ function SameSchema({ leftHandle, rightHandle, showTitle }: TabProps) {
   )
 }
 
-function DifferentSchema({ leftHandle, rightHandle, showTitle }: TabProps) {
+function DifferentSchema({ leftHandle, rightHandle, showTitle, showDebug }: TabProps) {
   return (
     <div>
       {showTitle && <h2>Different Schema</h2>}
       <div id="editors">
         <div className="editor">
-          <Editor
+          <DebugEditor
             name="left"
             handle={leftHandle}
             path={["text"]}
             schemaAdapter={paragraphAndHeadingSchemaAdapter}
+            debug={showDebug}
           />
         </div>
         <div className="editor">
-          <Editor
+          <DebugEditor
             name="right"
             handle={rightHandle}
             path={["text"]}
             schemaAdapter={paragraphAndListItemsSchemaAdapter}
+            debug={showDebug}
           />
         </div>
       </div>
     </div>
   )
+}
+
+function DebugEditor({
+  name,
+  handle,
+  path,
+  schemaAdapter,
+  debug,
+}: {
+  name?: string
+  handle: DocHandle<unknown>
+  path: Automerge.Prop[]
+  schemaAdapter: SchemaAdapter
+  debug?: boolean
+}) {
+  const [spans, setSpans] = useState(Automerge.spans(handle.doc(), path))
+  handle.on("change", (payload) => {
+    setSpans(Automerge.spans(payload.doc, ["text"]))
+  })
+  const [editorState, setEditorState] = useState<EditorState | undefined>(undefined);
+  function handleEditorStateChange(state: EditorState) {
+    setEditorState(state);
+  }
+
+  return (
+    <div>
+      <Editor
+        name={name}
+        handle={handle}
+        path={path}
+        schemaAdapter={schemaAdapter}
+        onStateChange={handleEditorStateChange}
+      />
+      {debug && (
+        <div style={{display: 'flex'}}>
+          <div>
+            <span>ProseMirror state</span>
+            <pre>
+              {JSON.stringify(editorState?.toJSON(), null, 2)}
+            </pre>
+          </div>
+          <div>
+            <span>Automerge state</span>
+            <pre>
+              {JSON.stringify(spans, null, 2)}
+            </pre>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+
 }
 
 const paragraphAndHeadingSchemaAdapter = new SchemaAdapter({
